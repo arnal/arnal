5a81f5ae92cc5eb5aafe7cd86c16aab9bbf27076
Jan Stransky <jan.stransky@arnal.cz>
2013-09-13 07:39:11 +0200

    Fix bootstrap default route

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -138,6 +138,6 @@ Kohana::modules(array(
  */
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
-		'controller' => 'welcome',
+		'controller' => 'index',
 		'action'     => 'index',
 	));
